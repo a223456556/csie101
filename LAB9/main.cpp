f1ae96105a4bf1ae8dda78f2d909687751e40aa0
a223456556 <81285047+a223456556@users.noreply.github.com>
2023-05-25 15:03:19 +0800

    Create main.cpp

---
+++ LAB9/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+class ReplaceMyString
+{
+    public:
+    ReplaceMyString()
+    {
+        ifstream inputFile(__FILE__);
+        ofstream outputFile("rmain.cpp");
+    }
+    void replaceString(string a,string b)
+    {
+        ifstream inputFile(__FILE__);
+        ofstream outputFile("rmain.cpp");
+        
+        if(inputFile.is_open() && outputFile.is_open())
+        {
+            string line;
+            while (getline(inputFile, line)) 
+            {
+                size_t P = line.find(a);
+                while (P != string::npos) 
+                {
+                    line.replace(P, a.length(), b);
+                    P = line.find(a, P + b.length());
+                }
+                outputFile << line << endl;
+            }
+            inputFile.close();
+            outputFile.close();
+        }
+        else
+        {
+            cout<<"fail"<<endl;
+        }
+        
+    }
+    
+};
+int main()
+{
+    ReplaceMyString my;
+    my.replaceString("IU","IU is best");
+    
+    return 0;
+}
