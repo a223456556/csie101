cdd01b7edf642c1182b410e5de16cc9286489893
a223456556 <81285047+a223456556@users.noreply.github.com>
2023-06-01 14:32:56 +0800

    Create main.cpp

---
+++ LAB10/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <vector>
+using namespace std;
+
+class myString
+{
+    private:
+        string m_str;
+    public:
+    myString (string s)
+    {
+        m_str = s;
+    }
+};
+
+class ReadClass
+{
+public:
+    void showClass()
+    {
+        ifstream file("main.cpp");
+        if (file.is_open())
+        {
+            string line;
+            while (getline(file, line))
+            {
+                smatch match;
+                if (regex_search(line, match, classNameRegex))
+                {
+                    string className = match[1].str();
+                    classNames.push_back(className);
+                }
+            }
+            file.close();
+        }
+
+        // 輸出每個類別的名字
+        for (const string& className : classNames)
+        {
+            cout <<"class "<< className << endl;
+        }
+    }
+
+private:
+    regex classNameRegex{"class\\s+(\\w+)"};
+    vector<string> classNames;
+};
+
+int main()
+{
+    ReadClass rfile;
+    rfile.showClass();
+
+    return 0;
+}
